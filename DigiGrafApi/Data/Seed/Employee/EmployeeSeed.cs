e6c16127b7cfbbb43963580e16ce43c83327f54d
PatrickSt1991 <patrick.stel@kpnmail.nl>
2026-03-04 13:41:42 +0100

    Match seed data

--- DigiGrafApi/Data/Seed/Employee/EmployeeSeed.cs
+++ DigiGrafApi/Data/Seed/Employee/EmployeeSeed.cs
@@ -1,20 +1,28 @@
-﻿namespace DigiGrafWeb.Data.Seed.Employee
+﻿using DigiGrafWeb.Models;
+using Microsoft.AspNetCore.Identity;
+
+namespace DigiGrafWeb.Data.Seed.Employee
 {
     public static class EmployeeSeed
     {
-        public static async Task SeedAsync(AppDbContext db)
+        public static async Task SeedAsync(AppDbContext db, UserManager<ApplicationUser> userManager)
         {
             if (db.Employees.Any())
                 return;
 
+            var user = await userManager.FindByEmailAsync("admin@digigraf.local");
+
             db.Employees.Add(new Models.Employee
             {
                 Id = Guid.NewGuid(),
-                FirstName = "Patrick",
-                LastName = "Stel",
-                Initials = "P.S.",
-                Email = "patrick@digigraf.local",
-                IsActive = true
+                IsActive = true,
+                Initials = "A.",
+                FirstName = "Admin",
+                LastName = "DigiGraf",  
+                BirthDate = DateOnly.FromDateTime(DateTime.Now),
+                Email = "admin@digigraf.local",
+                StartDate = DateOnly.FromDateTime(DateTime.Now),
+                UserId = user?.Id
             });
 
             await db.SaveChangesAsync();
--- DigiGrafApi/Data/Seed/SeedData.cs
+++ DigiGrafApi/Data/Seed/SeedData.cs
@@ -5,6 +5,8 @@ using DigiGrafWeb.Data.Seed.Example;
 using DigiGrafWeb.Data.Seed.Insurance;
 using DigiGrafWeb.Data.Seed.Reference;
 using DigiGrafWeb.Data.Seed.Supplier;
+using DigiGrafWeb.Models;
+using Microsoft.AspNetCore.Identity;
 
 namespace DigiGrafWeb.Data.Seed
 {
@@ -14,10 +16,11 @@ namespace DigiGrafWeb.Data.Seed
         {
             using var scope = services.CreateScope();
             var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+            var userManager = scope.ServiceProvider.GetRequiredService<UserManager<ApplicationUser>>();
 
             await RoleSeed.SeedAsync(scope.ServiceProvider);
             await UserSeed.SeedAsync(scope.ServiceProvider);
-            await EmployeeSeed.SeedAsync(db);
+            await EmployeeSeed.SeedAsync(db, userManager);
 
             await SalutationSeed.SeedAsync(db);
             await BodyFindingSeed.SeedAsync(db);
